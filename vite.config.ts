d994347062fcca1da15f862eea317820341d481e
Ashwet Kini <ashukini3@gmail.com>
2025-08-07 03:03:01 +0530

    Deploy built site with correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kinisoftwares.github.io/',  // 👈 ADD THIS LINE
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
